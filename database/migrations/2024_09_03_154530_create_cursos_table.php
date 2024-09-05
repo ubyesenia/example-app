0d4957b6519c434c6b9a00b85d8eecf05149a3b1
Yesenia Urriola <ubyesenia@gmail.com>
2024-09-05 18:01:50 -0400

    Cree datos de prueba a tablas Curso y User con Factory y Seeder

--- database/migrations/2024_09_03_154530_create_cursos_table.php
+++ database/migrations/2024_09_03_154530_create_cursos_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
             $table->text('description');
+            $table->text('category');            
+            $table->timestamps();
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Curso;
+use App\Models\User;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       // $this->call(CursoSeeder::class);
+       User::factory(10)->create();
+       Curso::factory(50)->create();
     }
 }
